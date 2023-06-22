a658b903e42bf711a5c951eb65cbea39f5d612ed
JesusE123 <Jesusan0110@gmail.com>
2023-06-22 11:30:19 -0300

    nodemailer

---
+++ pages/api/contact.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method != "POST") {
+
+    return res.status(405).json({ error: 'Method Not Allowed' });
+}
+
+    const { email, subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "jesusan0110@gmail.com",
+        
+      },
+    });
+
+    const contentMail = {
+      from: email,
+      to: "jesusan0110@gmail.com",
+      subject: subject,
+      text: message,
+    };
+
+    try {
+      await transporter.sendMail(contentMail);
+      return res.status(200).json({ hola: "mundo" });
+    } catch (error) {
+      return res.status(400).json({ error: "por aca no papa" });
+    }
+  }
+
+
+export default handler;
--- src/app/api/contact/route.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-
-
-export async function POST(req:NextApiRequest, res:NextApiResponse) {
-   const {email, subject, message} =  req.body;
-
-   console.log(req.body)
-}
-
